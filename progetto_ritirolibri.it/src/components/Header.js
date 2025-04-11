dfb20c32b8948d1a44eb62d9bb5f08a0f48972ee
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-04-11 10:16:00 +0200

    Preparazione per il push della 1.0

--- progetto_ritirolibri.it/src/components/Header.js
+++ progetto_ritirolibri.it/src/components/Header.js
@@ -1,15 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useState } from "react";
+import { FaHome } from "react-icons/fa";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
-export default function Header() {
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">Chi siamo</Link></li>
-          <li><Link href="/contact">Contatti</Link></li>
-        </ul>
-      </nav>
+      <div className="container">
+        <Link href="/">
+          <FaHome size={24} />
+        </Link>
+        <button onClick={toggleMenu}>
+          <Bars3Icon className="icon" height={24} />
+        </button>
+      </div>
+      {isMenuOpen && (
+        <nav>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">Chi Siamo</Link></li>
+            <li><Link href="/contact">Contatti</Link></li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
-}
+};
+
+export default Header;
